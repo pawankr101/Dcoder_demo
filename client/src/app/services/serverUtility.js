c648341a845f75fb5beac718d9c3f4b7a56df753
akshay <akshay@example.com>
2019-07-13 19:55:43 +0530

    code refactor

--- client/src/app/services/serverUtility.js
+++ client/src/app/services/serverUtility.js
@@ -1,83 +1,82 @@
-import { UtilityService as utility } from "./utility";
+import { UtilityService } from "./utility";
 
-class ServerUtilityService {
-    localConst = { };
+export class ServerUtilityService {
+	localConst = {};
+	constructor() {
+		this.utility = new UtilityService();
+	}
 
-    buildParam(params) {
-        let paramString = '';
-        utility.forLoop(params, (param, key) => {
-            if(utility.isString(param)) {
-                paramString += `${key}=${param}`
-            }
-        });
-        return paramString ? `?${paramString}` : '';
-    }
+	buildParam(params) {
+		let paramString = '';
+		this.utility.forLoop(params, (param, key) => {
+			if(this.utility.isString(param)) {
+				paramString += `${key}=${param}`
+			}
+		});
+		return paramString ? `?${paramString}` : '';
+	}
 
-    buildHeader(commonHeader, headers) {
-        commonHeader = commonHeader || {};
-        utility.forLoop(headers, (header, key) => {
-            if(utility.isString(header)) {
-                commonHeader[key] = header
-            }
-        });
-        return commonHeader;
-    }
-    
-    getRequest(apiEndUrl, options) {
-        return new Promise((resolve, reject) => {
-            if(!utility.isString(apiEndUrl)) {
-                reject({
-                    message: "'apiEndUrl' must be string"
-                });
-            }
-            let url = apiEndUrl + this.buildParam(utility.getValue(options, 'params')),
-                headers = {
-                    'Content-Type': 'application/json'
-                };
-            headers = this.buildHeader(headers, utility.getValue(options, 'headers'))
-            fetch(url, {
-                method: 'GET',
-                headers: headers
-            }).then(result => {
-                return result.json();
-            }).then(resultData => {
-                resolve(resultData);
-            }).catch(error => {
-                reject(error);
-            });
-        });
-    }
-    
-    postRequest(apiEndUrl, payload, options) {
-        return new Promise((resolve, reject) => {
-            if(!utility.isString(apiEndUrl)) {
-                reject({
-                    message: "'apiEndUrl' must be string"
-                });
-            }
-            if(!utility.isDefinedAndNotNull(payload)) {
-                payload = {};
-            }
-            let url = apiEndUrl + this.buildParam(utility.getValue(options, 'params')),
-                headers = {
-                    'Content-Type': 'application/json'
-                };
-            headers = this.buildHeader(headers, utility.getValue(options, 'headers'))
-            fetch(url, {
-                method: 'POST',
-                headers: headers,
-                body: JSON.stringify(payload)
-            }).then(result => {
-                return result.json();
-            }).then(resultData => {
-                resolve(resultData);
-            }).catch(error => {
-                reject(error);
-            });
-        });
-    }
-}
+	buildHeader(commonHeader, headers) {
+		commonHeader = commonHeader || {};
+		this.utility.forLoop(headers, (header, key) => {
+			if(this.utility.isString(header)) {
+				commonHeader[key] = header
+			}
+		});
+		return commonHeader;
+	}
 
-export default {
-    ServerUtilityService: new ServerUtilityService()
+	getRequest(apiEndUrl, options) {
+		return new Promise((resolve, reject) => {
+			if(!this.utility.isString(apiEndUrl)) {
+				reject({
+					message: "'apiEndUrl' must be string"
+				});
+			}
+			let url = apiEndUrl + this.buildParam(this.utility.getValue(options, 'params')),
+				headers = {
+					'Content-Type': 'application/json'
+				};
+			headers = this.buildHeader(headers, this.utility.getValue(options, 'headers'))
+			fetch(url, {
+				method: 'GET',
+				headers: headers
+			}).then(result => {
+				return result.json();
+			}).then(resultData => {
+				resolve(resultData);
+			}).catch(error => {
+				reject(error);
+			});
+		});
+	}
+
+	postRequest(apiEndUrl, payload, options) {
+		return new Promise((resolve, reject) => {
+			if(!this.utility.isString(apiEndUrl)) {
+				reject({
+					message: "'apiEndUrl' must be string"
+				});
+			}
+			if(!this.utility.isDefinedAndNotNull(payload)) {
+				payload = {};
+			}
+			let url = apiEndUrl + this.buildParam(this.utility.getValue(options, 'params')),
+				headers = {
+					'Content-Type': 'application/json'
+				};
+			headers = this.buildHeader(headers, this.utility.getValue(options, 'headers'))
+			fetch(url, {
+				method: 'POST',
+				headers: headers,
+				body: JSON.stringify(payload)
+			}).then(result => {
+				return result.json();
+			}).then(resultData => {
+				resolve(resultData);
+			}).catch(error => {
+				reject(error);
+			});
+		});
+	}
 }
