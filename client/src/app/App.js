d9e5f9c1acd3b42142c220a1551cc5f64bcbc980
akshay <akshay@example.com>
2019-07-16 23:53:27 +0530

    added routes

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -5,6 +5,17 @@ import Login from './components/login/login'
 import Register from './components/register/register';
 import { UtilityService } from './services/utility';
 
+function ThreadsHome (props) {
+	return (
+		<React.Fragment>
+			<Route exact path="/threads" render={(props) => <h1>threads</h1>}/>
+			<Route exact strict path="/threads/" render={() => <Redirect to="/threads"/>}/>
+			<Route exact path="/threads/new" render={(props) => <h1>new threads</h1>}/>
+			<Route exact strict path="/threads/new/" render={() => <Redirect to="/threads/new"/>}/>
+		</React.Fragment>
+	);
+}
+
 class App extends React.Component {
 	constructor() {
 		super();
@@ -21,9 +32,18 @@ class App extends React.Component {
 						return <Redirect to="/login"/>
 					}
 				}}/>
-				<Route exact strict path="/threads" render={(props) => <h1>threads</h1>}/>
-				<Route exact strict path="/login" render={(props) => <Login {...props}/>}/>
-				<Route exact strict path="/register" render={(props) => <Register {...props}/>}/>
+				<Route path="/threads" render={(props) => {
+					const jwt = this.utility.getFromLocalStorage('jwt_token');
+					if(jwt) {
+						return <ThreadsHome {...props}/>
+					} else {
+						return <Redirect to="/login"/>
+					}
+				}}/>
+				<Route exact path="/login" render={(props) => <Login {...props}/>}/>
+				<Route exact strict path="/login/" render={() => <Redirect to="/login"/>}/>
+				<Route exact path="/register" render={(props) => <Register {...props}/>}/>
+				<Route exact strict path="/register/" render={() => <Redirect to="/register"/>}/>
 			</div>
 		);
 	}
